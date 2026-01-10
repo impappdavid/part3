4ba258395efaad53c6d9f1d27bc3879ead44b9b6
Papp Dávid <impappdavid@gmail.com>
2026-01-10 11:04:38 +0100

    Fixing the 3.11 part frontend

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 var morgan = require('morgan')
-const cors = require('cors')
 
 const app = express();
 app.use(express.json());
+app.use(express.static('dist'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
-app.use(express.static('dist'))
+
 
 let persons = [
     {
