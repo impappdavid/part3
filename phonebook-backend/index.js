a81a54ec540e924fcfcb99a5289e54273e05f605
Papp Dávid <impappdavid@gmail.com>
2026-01-11 11:55:39 +0100

    Making the 3.18 part

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -10,29 +10,6 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
@@ -41,11 +18,6 @@ const errorHandler = (error, request, response, next) => {
     }
 
     next(error)
-}
-
-const generateId = () => {
-    const randomId = Math.floor(Math.random() * 1000000)
-    return String(randomId)
 }
 
 app.get('/api/persons', (request, response, next) => {
@@ -78,12 +50,6 @@ app.post('/api/persons', (request, response, next) => {
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number is missing'
-        });
-    }
-    const nameExists = persons.find(person => person.name === body.name);
-    if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
         });
     }
     const person = new Person({
