1db524899d1a517d9cd9b24a11f71d8573c6c92d
Papp Dávid <impappdavid@gmail.com>
2026-01-11 11:46:29 +0100

    Making the 3.16 part

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -33,6 +33,16 @@ let persons = [
     }
 ]
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 const generateId = () => {
     const randomId = Math.floor(Math.random() * 1000000)
     return String(randomId)
@@ -42,6 +52,7 @@ app.get('/api/persons', (request, response) => {
     Person.find().then(person => {
         response.json(person)
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', async (request, response) => {
@@ -58,6 +69,7 @@ app.get('/api/persons/:id', (request, response) => {
             response.status(404).end()
         }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -82,14 +94,17 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     Person.findByIdAndDelete(request.params.id).then(result => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
+app.use(errorHandler)
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
